8684a5a49efc695c483d5765937a13762bb76c49
cinzinga <34584134+cinzinga@users.noreply.github.com>
2021-04-12 17:10:14 -0400

    Create 00-BaseCode.cs

---
+++ 00-BaseCode.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvasionPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Evasion code here
+            runner();
+        }
+        static void runner()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
